bb969bca8b6ccb3cc2de33db688eb5f9f025f64d
AmadeoNeto <amadeotatocota@gmail.com>
2024-03-06 00:00:29 -0300

    New affirm and deny utterances and qa function

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def query_builder(genres=None,
                     platforms=None,
                     developer=None,
@@ -38,3 +40,32 @@ def query_builder(genres=None,
     query += ')'
 
     return query
+    
+
+def generate_context_string(row):
+    return ' | '.join([f"{col}: {val}" for col, val in row.items()])
+
+
+def generate_qa_context(game_title, connection):
+    query = f'''SELECT gi.name, gi.release_date, gi.developer, gi.publisher, gi.platforms,
+           gi.categories, gi.genres, gi.steamspy_tags, gi.price, d.short_description 
+           FROM description d, game_info gi 
+           WHERE d.steam_appid = gi.appid
+           AND gi.name LIKE \'%{game_title}%\' '''
+    
+
+    result_df = pd.read_sql_query(query, connection)
+
+    # Apply the function to each row and concatenate the results
+    if len(result_df) > 0:
+        context = result_df.apply(generate_context_string, axis=1)
+        context = context.values[0]
+    else:
+        context = ""
+
+    return context
+
+import sqlite3
+path_to_game_info_db = r"C:\Users\Amadeo\Documents\_UFPE\Eletivas\PLN\chatbot\steam_chatbot\steam_base.db"
+conn = sqlite3.connect(path_to_game_info_db)
+print(generate_qa_context(None,conn))
